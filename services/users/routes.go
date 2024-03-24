86753764f541d42faa21b8a35b9d889bfb420139
Kei-K23 <keiksl2301@gmail.com>
2024-03-24 19:05:01 +0630

    add utils for wparse json, write json and write error

--- services/users/routes.go
+++ services/users/routes.go
@@ -3,6 +3,8 @@ package users
 import (
 	"net/http"
 
+	"github.com/Kei-K23/go-ecom/types"
+	"github.com/Kei-K23/go-ecom/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -22,4 +24,14 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+
+	// get JSON payload
+	var payload types.RegisterUserPayload
+
+	// parse payload to json
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
 }
---
+++ utils/utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ParseJSON(r *http.Request, payload any) error {
+	if r.Body == nil {
+		return fmt.Errorf("missing request payload")
+	}
+
+	return json.NewDecoder(r.Body).Decode(payload)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	WriteJSON(w, status, map[string]string{"error": err.Error()})
+}
